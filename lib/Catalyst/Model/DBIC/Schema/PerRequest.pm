b9d88e617540ad9a479095d9bd0621f141dc07ae
moltar <romanf@cpan.org>
2015-02-14 15:14:58 -0500

    Initial commit.

---
+++ lib/Catalyst/Model/DBIC/Schema/PerRequest.pm
@@ -0,0 +1,10 @@
+package Catalyst::Model::DBIC::Schema::PerRequest;
+
+# ABSTRACT: Catalyst::Model::DBIC::Schema::PerRequest
+
+use strict;
+use warnings;
+
+use version; our $VERSION = version->new('v1.0.0');
+
+1; ## eof
